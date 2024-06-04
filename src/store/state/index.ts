0ee7ee5ca3ba031ecca91b1b288dfd6b9252cbb4
张世纪 <zhangshiji@mygolbs.com>
2024-06-04 15:18:01 +0800

    localStorage

--- src/store/state/index.ts
+++ src/store/state/index.ts
@@ -1,11 +1,9 @@
-"use client"
+"use client";
 const initState = {
   collapsed: false, // 菜单收起
   theme: {
-    colorPrimary: "#1677ff",
+    colorPrimary: localStorage.getItem("themeConfig") || "#1677ff",
   },
 };
-if(typeof window !== "undefined") {
-  initState.theme.colorPrimary = localStorage.getItem("themeConfig") || "#1677ff"
-}
+
 export default initState;
